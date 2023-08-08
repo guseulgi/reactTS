e3a274adc10852106ee9ca8734514f7922a08b0c
seulgi <guseul9405@gmail.com>
2023-08-08 22:37:38 +0900

    craco 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counter from './components/Counter';
-import Input from './components/Input';
-import Props from './components/Props';
-import Todo from './components/Todo';
-import Header from './components/Header';
-import Section from './components/Section';
-import Tail from './components/Tail';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Props from "./components/Props";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Tail from "./components/Tail";
 
 export interface MyProps {
-  name : string,
-  age : number,
-  hobbies : string[],
+  name: string;
+  age: number;
+  hobbies: string[];
 }
 
 function App() {
-  const p:MyProps = {
-    name : 'kim',
-    age : 20,
-    hobbies: ['1', '2'],
-  }
+  const p: MyProps = {
+    name: "kim",
+    age: 20,
+    hobbies: ["1", "2"],
+  };
 
   return (
     <>
       <Header />
       <Tail />
-      {/* <Section color='blue' /> */}
-      {/* <Counter /> */}
-      {/* <Input /> */}
-      {/* <Props {...p} /> */}
-      {/* <Todo /> */}
+      <Section color="blue" />
+      <Counter />
+      <Input />
+      <Props {...p} />
+      <Todo />
     </>
   );
 }
